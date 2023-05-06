d70ea98b7be3e485aab62021499c99cb7925f793
Ivan Mutsko <imucko1996@gmail.com>
2023-05-06 13:49:53 +0300

    add query functions

---
+++ src/api/api.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'bc10706c6695cbc040a40fff81bfdeeb';
+
+export const fetchTrendMovies = async () => {
+  try {
+    const { data } = await axios.get(
+      `${API_URL}/trending/all/week?api_key=${API_KEY}`
+    );
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const fetchSearchMovies = async searchQuery => {
+  const params = {
+    language: 'en-US',
+    page: 1,
+    include_adult: false,
+    query: searchQuery,
+  };
+
+  try {
+    const { data } = await axios.get(
+      `${API_URL}/search/movie?api_key=${API_KEY}&${params}`
+    );
+
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const fetchMovieById = async movieId => {
+  try {
+    const { data } = await axios.get(
+      `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`
+    );
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const fetchMovieCredits = async movieId => {
+  try {
+    const { data } = await axios.get(
+      `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
+    );
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const fetchMovieReviews = async movieId => {
+  try {
+    const { data } = await axios.get(
+      `${API_URL}movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US`
+    );
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { api } from '../api/api';
+
 export const App = () => {
   return (
     <div
